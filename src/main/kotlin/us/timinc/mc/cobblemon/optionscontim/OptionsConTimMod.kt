3515ef8c2a207f8743927b64c3c398ef0d3ef00a
tmetcalfe89 <tmetcalfe89@gmail.com>
2025-06-09 23:35:01 -0700

    prototype

---
+++ src/main/kotlin/us/timinc/mc/cobblemon/optionscontim/OptionsConTimMod.kt
@@ -0,0 +1,29 @@
+package us.timinc.mc.cobblemon.optionscontim
+
+import com.cobblemon.mod.common.api.events.CobblemonEvents
+import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
+import us.timinc.mc.cobblemon.optionscontim.config.ConfigBuilder
+import us.timinc.mc.cobblemon.optionscontim.config.OptionsConTimConfig
+
+object OptionsConTimMod : ModInitializer {
+    @Suppress("MemberVisibilityCanBePrivate")
+    const val MOD_ID = "optionscontim"
+
+    @Suppress("MemberVisibilityCanBePrivate")
+    lateinit var config: OptionsConTimConfig
+
+    init {
+        CobblemonEvents.THROWN_POKEBALL_HIT.subscribe { evt ->
+            if (!evt.pokemon.isBattling && !config.outOfBattleCaptures) evt.cancel()
+        }
+    }
+
+    override fun onInitialize() {
+        config = ConfigBuilder.load(OptionsConTimConfig::class.java, MOD_ID)
+
+        ServerLifecycleEvents.END_DATA_PACK_RELOAD.register { _, _, _ ->
+            config = ConfigBuilder.load(OptionsConTimConfig::class.java, MOD_ID)
+        }
+    }
+}
